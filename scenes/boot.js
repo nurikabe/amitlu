d86087288c419e72057c2f44b1f9eb24ef992b91
nurikabe <nurikabe@yahoo.com>
2019-01-18 16:09:37 -0500

    Title screen graphic for the boot scene

--- scenes/boot.js
+++ scenes/boot.js
@@ -9,25 +9,32 @@ var BootScene = new Phaser.Class({
 
     preload: function ()
     {
+        this.load.image('boot', 'assets/boot.png');
+
         this.load.audio('bump', 'assets/doorClose_4.ogg');
 
-        // map tiles
+        // Map tiles
         this.load.image('tiles', 'assets/map/spritesheet.png');
-
-        // map in json format
         this.load.tilemapTiledJSON('map', 'assets/map/map.json');
 
-        // our two characters
+        // Main characters
         this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
 
-        // enemies
-        //this.load.image('dragonblue', 'assets/dragonblue.png');
-        //this.load.image('dragonorrange', 'assets/dragonorrange.png');
+        // Enemies
+        this.load.spritesheet('santa', 'assets/santa.png', { frameWidth: 27, frameHeight: 29 });
+        //this.load.spritesheet('totemFire', 'assets/totem_fire.png');
     },
 
     create: function ()
     {
+        var bootImage = this.add.image(160, 120, 'boot');
+        //bootImage.setScale(0.18);
+        bootImage.displayHeight = 250;
+        bootImage.displayWidth = 350;
+
         this.sound.add('bump');
-        this.scene.start('WorldScene');
+
+        this.add.text(38, 150, 'Press Any Key to Start', { fontSize: '18px', fill: '#fff' });
+        this.input.keyboard.on('keydown', function () { this.scene.start('WorldScene'); }, this);
     }
 });
